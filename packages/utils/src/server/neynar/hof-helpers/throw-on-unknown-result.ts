4400c18e59de4747fedc21450f3e146a180de52f
jeffrey-computers <computers@thefirm.biz>
2025-04-30 13:00:10 +0100

    use full if declaration

--- packages/utils/src/server/neynar/hof-helpers/throw-on-unknown-result.ts
+++ packages/utils/src/server/neynar/hof-helpers/throw-on-unknown-result.ts
@@ -1,4 +1,6 @@
 export function throwOnUnknownResult<T>(result: T, context = 'call'): T {
-  if (!result) throw new Error(`no result for ${context}`);
+  if (!result) {
+    throw new Error(`no result for ${context}`);
+  }
   return result;
 }
