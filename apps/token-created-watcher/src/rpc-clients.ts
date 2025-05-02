87c71fd3b4c1a83134073413f5075f473103affe
Ray K. Back <ray@thefirm.biz>
2025-05-02 15:13:01 +0200

    lint

--- apps/token-created-watcher/src/rpc-clients.ts
+++ apps/token-created-watcher/src/rpc-clients.ts
@@ -3,11 +3,17 @@ import {
   createWebsocketPublicClient
 } from '@wiretap/utils/shared';
 import { env } from './env.js';
+import type { HttpTransport, PublicClient, WebSocketTransport } from 'viem';
+import { base } from 'viem/chains';
 
-export const websocketPublicClient = createWebsocketPublicClient({
+export const websocketPublicClient: PublicClient<
+  WebSocketTransport,
+  typeof base
+> = createWebsocketPublicClient({
   alchemyApiKey: env.ALCHEMY_API_KEY
 });
 
-export const httpPublicClient = createHttpPublicClient({
-  alchemyApiKey: env.ALCHEMY_API_KEY
-});
+export const httpPublicClient: PublicClient<HttpTransport, typeof base> =
+  createHttpPublicClient({
+    alchemyApiKey: env.ALCHEMY_API_KEY
+  });
