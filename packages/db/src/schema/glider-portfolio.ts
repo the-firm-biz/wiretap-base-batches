84e8df34cece94eaa5f6274c9c1992aafc2a2a17
jeffrey-computers <computers@thefirm.biz>
2025-05-07 16:46:59 +0100

    Add 'Targets' and 'GliderPortfolio' schema

---
+++ packages/db/src/schema/glider-portfolio.ts
@@ -0,0 +1,34 @@
+import {
+  pgTable,
+  serial,
+  timestamp,
+  integer,
+  uniqueIndex,
+  text
+} from 'drizzle-orm/pg-core';
+import { wireTapAccounts } from './accounts/wire-tap-accounts.js';
+
+export const gliderPortfolios = pgTable(
+  'glider_portfolios',
+  {
+    id: serial('id').primaryKey(),
+    /** WireTap Account that created the Glider Portfolio */
+    wireTapAccountId: integer('wire_tap_account_id')
+      .notNull()
+      .references(() => wireTapAccounts.id),
+    /** Glider's unique Portfolio ID */
+    portfolioId: integer('portfolio_id').notNull().unique(),
+    /** Session Key Account address for the Glider Portfolio */
+    address: text('address').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at')
+  },
+  (table) => [
+    uniqueIndex('glider_portfolio_wire_tap_account_id_unique').on(
+      table.wireTapAccountId
+    )
+  ]
+);
+
+export type GliderPortfolio = typeof gliderPortfolios.$inferSelect;
+export type NewGliderPortfolio = typeof gliderPortfolios.$inferInsert;
