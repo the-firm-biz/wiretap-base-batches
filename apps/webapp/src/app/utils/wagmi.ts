3a17ab01fb194adec8f005f2ff6924a9bd387da1
Jeffrey (Computer Operator) <162481663+jeffrey-computers@users.noreply.github.com>
2025-05-08 13:11:08 +0100

    base avatar & ens avatar for wallet icon (#34)
    * base avatar & ens avatar for wallet icon
    
    * rn, refactor

--- apps/webapp/src/app/utils/wagmi.ts
+++ apps/webapp/src/app/utils/wagmi.ts
@@ -1,6 +1,6 @@
 import { cookieStorage, createStorage } from '@wagmi/core';
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
-import { base, baseSepolia } from '@reown/appkit/networks';
+import { base, baseSepolia, mainnet } from '@reown/appkit/networks';
 import { clientEnv } from '@/clientEnv';
 
 export const wagmiAdapter = new WagmiAdapter({
@@ -8,7 +8,7 @@ export const wagmiAdapter = new WagmiAdapter({
     storage: cookieStorage
   }),
   projectId: clientEnv.NEXT_PUBLIC_REOWN_PROJECT_ID,
-  networks: [base, baseSepolia]
+  networks: [base, baseSepolia, mainnet] // mainnet used for ENS resolution
 });
 
 export const wagmiConfig = wagmiAdapter.wagmiConfig;
