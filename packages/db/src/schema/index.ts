91ee6956c8866ac3c8db0348f70d34c8b1ac60b4
jeffrey-computers <computers@thefirm.biz>
2025-05-09 12:38:17 +0100

    Refactor 'targets' table > 'trackedAccountEntities'

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -2,7 +2,7 @@ export * from './accounts/index.js';
 export * from './blocks.js';
 export * from './contracts.js';
 export * from './glider-portfolio.js';
-export * from './targets.js';
+export * from './accounts/tracked-account-entities.js';
 export * from './tokens.js';
 export * from './verification-sources.js';
 export * from './wire-tap-session-keys.js';
