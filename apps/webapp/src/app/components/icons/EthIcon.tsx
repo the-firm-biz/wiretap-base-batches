5c6f54c85f98ff6f81caf44715b1f95402d9468f
jeffrey-computers <computers@thefirm.biz>
2025-05-09 20:12:46 +0100

    update eth icon ids

--- apps/webapp/src/app/components/icons/EthIcon.tsx
+++ apps/webapp/src/app/components/icons/EthIcon.tsx
@@ -2,43 +2,43 @@ import { Icon, type IconProps } from './Icon';
 
 export const EthIcon = (props: IconProps) => (
   <Icon {...props}>
-    <g id="GROUP">
+    <g id="eth_icon_GROUP">
       <path
-        id="VECTOR"
+        id="eth_icon_VECTOR"
         d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z"
         fill="#627EEA"
       />
-      <g id="GROUP_2">
+      <g id="eth_icon_GROUP_2">
         <path
-          id="VECTOR_2"
+          id="eth_icon_VECTOR_2"
           d="M16.498 4V12.87L23.995 16.22L16.498 4Z"
           fill="white"
           fillOpacity="0.602"
         />
         <path
-          id="VECTOR_3"
+          id="eth_icon_VECTOR_3"
           d="M16.498 4L9 16.22L16.498 12.87V4Z"
           fill="white"
         />
         <path
-          id="VECTOR_4"
+          id="eth_icon_VECTOR_4"
           d="M16.498 21.9692V27.9962L24 17.6172L16.498 21.9692Z"
           fill="white"
           fillOpacity="0.602"
         />
         <path
-          id="VECTOR_5"
+          id="eth_icon_VECTOR_5"
           d="M16.498 27.9962V21.9682L9 17.6172L16.498 27.9962Z"
           fill="white"
         />
         <path
-          id="VECTOR_6"
+          id="eth_icon_VECTOR_6"
           d="M16.498 20.5731L23.995 16.2201L16.498 12.8721V20.5731Z"
           fill="white"
           fillOpacity="0.2"
         />
         <path
-          id="VECTOR_7"
+          id="eth_icon_VECTOR_7"
           d="M9 16.2201L16.498 20.5731V12.8721L9 16.2201Z"
           fill="white"
           fillOpacity="0.602"
