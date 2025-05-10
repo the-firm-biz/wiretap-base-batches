a4fdf95d8921526231d6161eb7e73965ea63e77e
jeffrey-computers <computers@thefirm.biz>
2025-05-10 14:18:55 +0100

    'get targets for authed account' API endpoint

--- apps/webapp/src/server/api/trpc-routers/wiretap-account-router/create-wiretap-account-router.ts
+++ apps/webapp/src/server/api/trpc-routers/wiretap-account-router/create-wiretap-account-router.ts
@@ -1,11 +1,13 @@
 import { createTRPCRouter } from '../../trpc';
 import { getGliderPortfolioForAuthedAccount } from './routes/get-glider-portfolio-for-authed-account';
+import { getTargetsForAuthedAccount } from './routes/get-targets-for-authed-account';
 
 /**
  * Routes specific to getting/setting data for the connected WireTap account
  */
 export const wireTapAccountRouter = createTRPCRouter({
-  getGliderPortfolioForAuthedAccount: getGliderPortfolioForAuthedAccount
+  getGliderPortfolioForAuthedAccount: getGliderPortfolioForAuthedAccount,
+  getTargetsForAuthedAccount: getTargetsForAuthedAccount
 });
 
 export type WireTapAccountRouter = typeof wireTapAccountRouter;
