eb4eea202013d176833093c7ed085604bdac5cb4
jeffrey-computers <computers@thefirm.biz>
2025-05-10 12:57:09 +0100

    rn

--- apps/webapp/src/serverEnv.ts
+++ apps/webapp/src/serverEnv.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 import { getEnv, secretString } from '@wiretap/utils/shared';
 
 const isDev = process.env.NODE_ENV === 'development';
-const optionalSecretString = isDev ? secretString.optional() : secretString;
+const optionalInDevSecretString = isDev
+  ? secretString.optional()
+  : secretString;
 
 const serverEnvSchema = z.object({
   NEYNAR_API_KEY: secretString,
@@ -12,13 +14,13 @@ const serverEnvSchema = z.object({
    * Optional strings in development
    */
   /** Glider API Key is only required if interacting with Glider API */
-  GLIDER_API_KEY: optionalSecretString,
+  GLIDER_API_KEY: optionalInDevSecretString,
   /** Validator private key only required if working with session keys */
-  KERNEL_VALIDATOR_PRIVATE_KEY: optionalSecretString,
+  KERNEL_VALIDATOR_PRIVATE_KEY: optionalInDevSecretString,
   /** ZeroDev RPC only required if working with session keys */
-  ZERODEV_RPC: optionalSecretString,
+  ZERODEV_RPC: optionalInDevSecretString,
   /** For secure storage of session keys */
-  SESSION_KEY_ENCRYPTION_SECRET: optionalSecretString
+  SESSION_KEY_ENCRYPTION_SECRET: optionalInDevSecretString
 });
 
 export type ServerEnv = z.infer<typeof serverEnvSchema>;
