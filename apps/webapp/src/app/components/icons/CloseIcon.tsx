dc89c0b06d963c1e7614949e40bfc32690b7c42e
GitHubBennetKensington <bennet@thefirm.biz>
2025-05-11 01:39:49 +0300

    fix dark mode icons

--- apps/webapp/src/app/components/icons/CloseIcon.tsx
+++ apps/webapp/src/app/components/icons/CloseIcon.tsx
@@ -4,7 +4,7 @@ export const CloseIcon = (props: IconProps) => (
   <Icon {...props} viewBox="0 0 24 24" fill="none">
     <path
       d="M18 6L6 18M6 6L18 18"
-      stroke="#262E29"
+      stroke="var(--foreground)"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
