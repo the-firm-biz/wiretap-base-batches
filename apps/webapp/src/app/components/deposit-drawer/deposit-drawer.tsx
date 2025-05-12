d39839950ecb84d12dc9477a91943940a861ecbf
Jeffrey (Computer Operator) <162481663+jeffrey-computers@users.noreply.github.com>
2025-05-12 09:45:12 +0100

    Deposit ETH form styles & validation (#59)

--- apps/webapp/src/app/components/deposit-drawer/deposit-drawer.tsx
+++ apps/webapp/src/app/components/deposit-drawer/deposit-drawer.tsx
@@ -5,16 +5,27 @@ import {
   Drawer
 } from '../ui/drawer';
 import { ReactNode } from 'react';
-import { DepositDrawerFundsSummary } from './deposit-drawer-funds-summary';
-import { textStyles } from '@/app/styles/template-strings';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
+import { DepositDrawerFundsSummary } from './funds-summary';
+import { EthDepositForm } from './eth-deposit-form';
+import { useAccount, useBalance } from 'wagmi';
+import { formatUnits } from '@/app/utils/format/format-units';
 
 interface DepositDrawerProps {
   trigger: ReactNode;
 }
 
 export function DepositDrawer({ trigger }: DepositDrawerProps) {
+  const { address } = useAccount();
+
+  const { data: balance } = useBalance({
+    address: address,
+    query: {
+      enabled: !!address
+    }
+  });
+
+  const balanceAsEth = formatUnits(balance?.value || BigInt(0), 18, 4);
+
   return (
     <Drawer>
       <DrawerTrigger asChild>{trigger}</DrawerTrigger>
@@ -24,28 +35,7 @@ export function DepositDrawer({ trigger }: DepositDrawerProps) {
           <div className="mt-6" />
           <DepositDrawerFundsSummary />
           <div className="mt-8" />
-          <div className="flex flex-col gap-2">
-            <p className={textStyles['compact-mid']}>Transfer ETH</p>
-            <Input inputMode="numeric" />
-          </div>
-          <div className="mt-[12px]" />
-          <div className="flex flex-row gap-1 w-full">
-            <Button variant="outline" className="flex-1">
-              25%
-            </Button>
-            <Button variant="outline" className="flex-1">
-              50%
-            </Button>
-            <Button variant="outline" className="flex-1">
-              75%
-            </Button>
-            <Button variant="outline" className="flex-1">
-              Max
-            </Button>
-          </div>
-          <div className="mt-8 flex w-full">
-            <Button className="flex-1">Confirm Deposit</Button>
-          </div>
+          <EthDepositForm userBalance={balanceAsEth} />
         </div>
       </DrawerContent>
     </Drawer>
