6e55dead9b2439cc0d8aa7d43b372788b1f0bf9d
jeffrey-computers <computers@thefirm.biz>
2025-05-13 14:44:07 +0100

    Minimal minitkit setup

--- apps/webapp/src/clientEnv.ts
+++ apps/webapp/src/clientEnv.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const clientEnvSchema = z.object({
   NEXT_PUBLIC_REOWN_PROJECT_ID: z.string(),
   NEXT_PUBLIC_ALCHEMY_API_KEY: z.string(),
+  NEXT_PUBLIC_ONCHAINKIT_API_KEY: z.string(),
   // For devx - validator address only required if working with session keys
   NEXT_PUBLIC_KERNEL_VALIDATOR_ADDRESS: z.string().optional()
 });
@@ -13,7 +14,8 @@ const clientEnvValues = {
   NEXT_PUBLIC_REOWN_PROJECT_ID: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
   NEXT_PUBLIC_ALCHEMY_API_KEY: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
   NEXT_PUBLIC_KERNEL_VALIDATOR_ADDRESS:
-    process.env.NEXT_PUBLIC_KERNEL_VALIDATOR_ADDRESS
+    process.env.NEXT_PUBLIC_KERNEL_VALIDATOR_ADDRESS,
+  NEXT_PUBLIC_ONCHAINKIT_API_KEY: process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY
 };
 
 // Doesn't make use of the getEnv util because of how NextJs handles client-side env variables
