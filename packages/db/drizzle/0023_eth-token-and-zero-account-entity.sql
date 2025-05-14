43afbce975a340a79fb1ab7e121d0eebbcd7849c
Ray K. Back <ray@thefirm.biz>
2025-05-14 19:15:28 +0200

    Add ETH as token and other required FK'd entities

---
+++ packages/db/drizzle/0023_eth-token-and-zero-account-entity.sql
@@ -0,0 +1,12 @@
+-- Custom SQL migration file, put your code below! --
+insert into account_entities(id, label)
+values (0, 'Vitalik');
+
+insert into contracts(id, address)
+values (0, '0x0000000000000000000000000000000000000000');
+
+insert into tokens(id, name, symbol, address, total_supply,
+                   deployment_contract_id, account_entity_id, deployment_transaction_hash,
+                   block)
+values (0, 'Ethereum', 'ETH', '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', 9223372036854775807 /*bigint::max*/,
+        0, 0, '0x0', 0)
