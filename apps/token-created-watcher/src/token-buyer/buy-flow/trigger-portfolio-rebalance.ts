8ab8bb286c939c3e68a25e1471584025c498fcd4
Ray K. Back <ray@thefirm.biz>
2025-05-15 17:58:10 +0200

    Add backoff calls

--- apps/token-created-watcher/src/token-buyer/buy-flow/trigger-portfolio-rebalance.ts
+++ apps/token-created-watcher/src/token-buyer/buy-flow/trigger-portfolio-rebalance.ts
@@ -5,15 +5,23 @@ import {
   type ServerlessDb,
   type ServerlessDbTransaction
 } from '@wiretap/db';
+import { callWithBackOff } from '@wiretap/utils/server';
 
 export async function triggerPortfolioRebalance(
   db: ServerlessDbTransaction | HttpDb | ServerlessDb,
   rebalanceId: number,
   portfolioId: string
 ): Promise<string> {
-  const triggerRebalanceRawResponse =
-    await triggerGliderPortfolioRebalance(portfolioId);
-  if (!triggerRebalanceRawResponse.success) {
+  const triggerRebalanceRawResponse = await callWithBackOff(
+    () => triggerGliderPortfolioRebalance(portfolioId),
+    undefined,
+    {
+      name: `triggerGliderPortfolioRebalance for portfolio ${portfolioId} ${rebalanceId}`
+    }
+  );
+  const success =
+    triggerRebalanceRawResponse && triggerRebalanceRawResponse.success;
+  if (!success) {
     await insertGliderPortfolioRebalanceLog(db, {
       gliderPortfolioRebalancesId: rebalanceId,
       label: 'TRIGGER_FAILED',
--- apps/token-created-watcher/src/token-buyer/buy-flow/update-portfolio-assets-ratio.ts
+++ apps/token-created-watcher/src/token-buyer/buy-flow/update-portfolio-assets-ratio.ts
@@ -2,37 +2,47 @@ import {
   type HttpDb,
   insertGliderPortfolioRebalanceLog,
   type ServerlessDb,
-  type ServerlessDbTransaction,
+  type ServerlessDbTransaction
 } from '@wiretap/db';
 import { updateGliderPortfolio } from '../glider-api/update-glider-portfolio.js';
 import type { Address } from 'viem';
+import { callWithBackOff } from '@wiretap/utils/server';
 
 type UpdatePortfolioAssetsRatioParams = {
   rebalanceId: number;
   portfolioId: string;
   accountEntityAddress: Address;
   tokenPercentageBps: number;
-  tokenAddress?: Address
+  tokenAddress?: Address;
 };
 
 export async function updatePortfolioAssetsRatio(
   db: ServerlessDbTransaction | HttpDb | ServerlessDb,
-  {
+  params: UpdatePortfolioAssetsRatioParams
+): Promise<void> {
+  const {
     rebalanceId,
     tokenPercentageBps,
     accountEntityAddress,
     portfolioId,
     tokenAddress
-  }: UpdatePortfolioAssetsRatioParams,
-): Promise<void> {
-  const updateRawResponse = await updateGliderPortfolio({
-    accountEntityAddress,
-    portfolioId: portfolioId,
-    tokenAddress: tokenAddress,
-    tokenPercentageBps
-  });
+  } = params;
+  const updateRawResponse = await callWithBackOff(
+    () =>
+      updateGliderPortfolio({
+        accountEntityAddress,
+        portfolioId: portfolioId,
+        tokenAddress: tokenAddress,
+        tokenPercentageBps
+      }),
+    undefined,
+    {
+      name: `updateGliderPortfolio for portfolio ${params}`
+    }
+  );
 
-  if (!updateRawResponse.success) {
+  const success = updateRawResponse && updateRawResponse.success;
+  if (!success) {
     await insertGliderPortfolioRebalanceLog(db, {
       gliderPortfolioRebalancesId: rebalanceId,
       label: 'UPDATE_FAILED',
