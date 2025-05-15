0fcd2b8c1284d2bb512e1f35216bcc2f3407ce11
jeffrey-computers <computers@thefirm.biz>
2025-05-15 10:40:03 +0100

    Add search and simple feed to discover page

--- apps/webapp/src/app/(pages)/(discover)/page.tsx
+++ apps/webapp/src/app/(pages)/(discover)/page.tsx
@@ -1,3 +1,18 @@
+import { SearchIcon } from '@/app/components/icons/SearchIcon';
+import TargetSearchDrawer from '@/app/components/target-search/target-search-drawer';
+import { Button } from '@/app/components/ui/button';
+
 export default function DiscoverPage() {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <TargetSearchDrawer
+        trigger={
+          <Button className="w-full" variant="outline">
+            <SearchIcon className="h-4 w-4" />
+            Farcaster username, wallet address...
+          </Button>
+        }
+      />
+    </div>
+  );
 }
