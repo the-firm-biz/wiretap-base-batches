b0df133713845f7322c0ca69231c87915e5c8fac
Jeffrey (Computer Operator) <162481663+jeffrey-computers@users.noreply.github.com>
2025-05-15 10:51:39 +0100

    Automatically focus input on drawer open (#104)

--- apps/webapp/src/app/components/ui/search-input.tsx
+++ apps/webapp/src/app/components/ui/search-input.tsx
@@ -2,28 +2,31 @@ import { textStyles } from '@/app/styles/template-strings';
 import { CloseIcon } from '../icons/CloseIcon';
 import { SearchIcon } from '../icons/SearchIcon';
 import { Input } from './input';
+import { forwardRef } from 'react';
 
 interface SearchInputProps extends React.ComponentProps<'input'> {
   onClear?: () => void;
 }
 
-export const SearchInput: React.FC<SearchInputProps> = ({
-  onClear,
-  ...props
-}) => {
-  const isEmpty = props.value === '';
-  return (
-    <div className="relative">
-      <SearchIcon className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2" />
-      <Input className={`${textStyles.compact} pl-7`} {...props} />
-      {onClear && !isEmpty && (
-        <button
-          className="absolute cursor-pointer right-2 top-1/2 h-4 w-4 -translate-y-1/2"
-          onClick={onClear}
-        >
-          <CloseIcon className="h-4 w-4" />
-        </button>
-      )}
-    </div>
-  );
-};
+export const SearchInput = forwardRef<HTMLInputElement, SearchInputProps>(
+  ({ onClear, ...props }, ref) => {
+    const isEmpty = props.value === '';
+
+    return (
+      <div className="relative">
+        <SearchIcon className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2" />
+        <Input ref={ref} className={`${textStyles.compact} pl-7`} {...props} />
+        {onClear && !isEmpty && (
+          <button
+            className="absolute cursor-pointer right-2 top-1/2 h-4 w-4 -translate-y-1/2"
+            onClick={onClear}
+          >
+            <CloseIcon className="h-4 w-4" />
+          </button>
+        )}
+      </div>
+    );
+  }
+);
+
+SearchInput.displayName = 'SearchInput';
