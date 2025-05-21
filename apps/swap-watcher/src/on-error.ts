05cde63438b4da5abf7d992e122512e6249af976
jeffrey-computers <computers@thefirm.biz>
2025-05-21 14:43:41 +0100

    Add observability to swap-watcher, improve observability when close to/hitting a fail state

--- apps/swap-watcher/src/on-error.ts
+++ apps/swap-watcher/src/on-error.ts
@@ -1,3 +1,6 @@
+import { sendSlackSystemMessage } from '@wiretap/utils/server';
+import { env } from './env.js';
+
 interface OnErrorParams {
   error: Error;
   startWatcher: () => void;
@@ -5,7 +8,7 @@ interface OnErrorParams {
 }
 
 let retryCount = 0;
-const MAX_RETRIES = 5;
+const MAX_RETRIES = 10;
 const RETRY_DELAY = 1000; // 1 second
 
 export function onError({ error, startWatcher, unwatch }: OnErrorParams) {
@@ -24,11 +27,34 @@ export function onError({ error, startWatcher, unwatch }: OnErrorParams) {
       `onError:: Attempting to reconnect... (Attempt ${retryCount}/${MAX_RETRIES})`
     );
 
+    sendSlackSystemMessage({
+      systemMessage: {
+        type: 'reconnectAttempt',
+        currentAttempt: retryCount,
+        maxAttempts: MAX_RETRIES
+      },
+      flyAppName: process.env.FLY_APP_NAME,
+      flyMachineId: process.env.FLY_MACHINE_ID,
+      botToken: env.SLACK_INFRABOT_TOKEN,
+      channelId: env.INFRA_NOTIFICATIONS_CHANNEL_ID
+    });
+
     // Retry after delay
     setTimeout(() => {
       startWatcher();
     }, RETRY_DELAY * retryCount);
   } else {
+    sendSlackSystemMessage({
+      systemMessage: {
+        type: 'reconnectMaxAttempts',
+        maxAttempts: MAX_RETRIES
+      },
+      flyAppName: process.env.FLY_APP_NAME,
+      flyMachineId: process.env.FLY_MACHINE_ID,
+      botToken: env.SLACK_INFRABOT_TOKEN,
+      channelId: env.INFRA_NOTIFICATIONS_CHANNEL_ID
+    });
+
     console.error(
       'onError:: Max retry attempts reached. Manual intervention required.'
     );
--- apps/token-created-watcher/src/start-watcher.ts
+++ apps/token-created-watcher/src/start-watcher.ts
@@ -4,12 +4,24 @@ import { onError } from './on-error.js';
 import { websocketPublicClient } from './rpc-clients.js';
 import { initPriceFeeds } from '@wiretap/utils/server';
 import type { WatchContractEventReturnType } from 'viem';
+import { sendSlackSystemMessage } from '@wiretap/utils/server';
+import { env } from './env.js';
 
 export async function startTokenCreatedWatcher() {
   let unwatch: WatchContractEventReturnType | null = null;
 
   const startWatcher = () => {
     console.log('Listening for token events...');
+
+    sendSlackSystemMessage({
+      systemMessage: {
+        type: 'startup'
+      },
+      flyAppName: process.env.FLY_APP_NAME,
+      flyMachineId: process.env.FLY_MACHINE_ID,
+      botToken: env.SLACK_INFRABOT_TOKEN,
+      channelId: env.INFRA_NOTIFICATIONS_CHANNEL_ID
+    });
 
     unwatch = websocketPublicClient.watchContractEvent({
       address: CLANKER_3_1_ADDRESS,
